526944a7af30b2633a2b059efb29985efb4fef01
Mark Perry <markperry@Marks-MacBook-Air.local>
2022-06-29 14:17:47 -0300

    made changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const menu_logo = document.querySelector("#menu-logo-image");
+const downArrow = document.querySelector("#nextbtn");
 
 menu_logo.addEventListener("click", function () {
   alert(screen.width + " " + screen.height);
 });
-console.log(menu_logo);
+console.log(downArrow);
