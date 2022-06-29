9c99c39cbfa0677cea86d496ccb4df4b623743b8
Mark Perry <markperry@Marks-MacBook-Air.local>
2022-06-29 07:11:30 -0300

    made .navbar fixed

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+const menu_logo = document.querySelector("#menu-logo-image");
 
+menu_logo.addEventListener("click", function () {
+  alert(screen.width + " " + screen.height);
+});
+console.log(menu_logo);
