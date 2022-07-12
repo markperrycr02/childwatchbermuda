b347822c9b45c8675289fe1e9d6b0af5c9a1aa91
Mark Perry <markperry@Marks-MacBook-Air.local>
2022-07-12 10:02:58 -0300

    change mobile button size

---
+++ main.js
@@ -0,0 +1,22 @@
+const img = document.querySelector("#hero-img");
+const hamburger = document.querySelector("#hamburg");
+const menu_close = document.querySelector("#menu-close-btn");
+const overlay = document.querySelector("#overlay");
+const learmore = document.querySelector("#learn-more-btn");
+
+hamburg.addEventListener("click", () => {
+  overlay.style.transform = "translate3d(0vw,0,0)";
+  // overlay.style.maxHeight = "100%";
+});
+
+learmore.addEventListener("click", () => {
+  alert("Learn More");
+});
+
+menu_close.addEventListener("click", () => {
+  overlay.style.transform = "translate3d(-100vw,0,0)";
+});
+
+img.addEventListener("click", () => {
+  alert(screen.width + " x " + screen.height);
+});
